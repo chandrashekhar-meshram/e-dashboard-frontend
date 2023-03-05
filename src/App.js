5156ac769063de9547183ac4d67029bb515f7f9f
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-05 19:51:12 +0530

    Routes made for each page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-      <BrowserRouter>
-      <Nav />
       <h1>E-Dashboard (Frontend)!</h1>
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+          <Route path="/" element={<h1>Product Listing Component</h1>} />
+          <Route path="/add" element={<h1>Add Product component</h1>} />
+          <Route path="/update" element={<h1>Update Product Component</h1>} />
+          <Route path="/logout" element={<h1>Logout Component</h1>} />
+          <Route path="/profile" element={<h1>Profile Component</h1>} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
