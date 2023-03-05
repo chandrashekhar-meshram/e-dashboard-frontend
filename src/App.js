a3de99cac7c2521dd49c12f8f34fb38d4922a949
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-05 19:27:49 +0530

    links using react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './style.css';
+import Nav from './Nav';
+import {BrowserRouter} from 'react-router-dom'
 
 export default function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Nav />
       <h1>E-Dashboard (Frontend)!</h1>
+      </BrowserRouter>
     </div>
   );
 }
