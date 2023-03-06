34ccbafec2840727b546f882ab6ec139d682854d
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-06 19:20:46 +0530

    signup page with link

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import SignUp from './components/SignUp';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-      <h1>E-Dashboard (Frontend)!</h1>
       <BrowserRouter>
         <Nav />
 
@@ -17,6 +17,7 @@ export default function App() {
           <Route path="/update" element={<h1>Update Product Component</h1>} />
           <Route path="/logout" element={<h1>Logout Component</h1>} />
           <Route path="/profile" element={<h1>Profile Component</h1>} />
+          <Route path='/signup' element={<SignUp />} />
         </Routes>
       </BrowserRouter>
       <Footer />
