78dee2c333462eaebbc0748dbba2fb70cc34fada
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-06 20:04:13 +0530

    SignUp page with style and state

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # e-dashboard-frontend
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/e-dashboard-frontend)
+
+* Click here to see output - https://e-dashboard-frontend.stackblitz.io/signup
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SignUp = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const collectData = ()=> {
+    console.log(name, email, password);
+  }
+
   return (
-    <div>
+    <div className='register'>
       <h1>Register</h1>
+      <input 
+        className='inputBox' 
+        placeholder='Name' 
+        type='text'
+        value={name}
+        onChange={(e)=> setName(e.target.value)}
+      />
+      
+      <input 
+        className='inputBox' 
+        placeholder='Email' 
+        type='text'
+        value={email}
+        onChange={(e)=> setEmail(e.target.value)}
+      />
+      
+      <input 
+        className='inputBox' 
+        placeholder='Password' 
+        type='password'
+        value={password}
+        onChange={(e)=> setPassword(e.target.value)}
+      />
+      
+      <button 
+        className='appButton'
+        type='button'
+        onClick={collectData}
+      > 
+      SignUp
+      </button>
     </div>
   );
 };
