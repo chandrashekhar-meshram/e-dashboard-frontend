7c366110f0fccbc86d46fcfdab378bf180b07d6d
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-07 17:57:16 +0530

    Private component is working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import SignUp from './components/SignUp';
+import PrivateComponent from './components/PrivateComponent';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -12,11 +13,15 @@ export default function App() {
         <Nav />
 
         <Routes>
-          <Route path="/" element={<h1>Product Listing Component</h1>} />
-          <Route path="/add" element={<h1>Add Product component</h1>} />
-          <Route path="/update" element={<h1>Update Product Component</h1>} />
-          <Route path="/logout" element={<h1>Logout Component</h1>} />
-          <Route path="/profile" element={<h1>Profile Component</h1>} />
+
+          <Route element={<PrivateComponent />}>
+              <Route path="/" element={<h1>Product Listing Component</h1>} />
+              <Route path="/add" element={<h1>Add Product component</h1>} />
+              <Route path="/update" element={<h1>Update Product Component</h1>} />
+              <Route path="/logout" element={<h1>Logout Component</h1>} />
+              <Route path="/profile" element={<h1>Profile Component</h1>} />
+          </Route>
+
           <Route path='/signup' element={<SignUp />} />
         </Routes>
       </BrowserRouter>
