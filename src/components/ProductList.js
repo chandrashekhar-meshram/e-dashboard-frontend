7c6b65bb2dfe31978b7e0121e18c1f128ad052f1
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-11 13:10:34 +0530

    26 Update Product Component UI - Add Link for update Product

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
 
 
 const ProductList = () => {
@@ -52,6 +53,7 @@ const ProductList = () => {
             <button type = 'button' onClick = {()=> deleteProduct(item._id)}>
               Delete
             </button> 
+            <Link to = {'/update/' + item._id}>Update</Link>
           </li>
         </ul>
       ))}
