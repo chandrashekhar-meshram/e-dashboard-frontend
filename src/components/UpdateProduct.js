14b45eddfbcfc46e640637a1205818c09efbadab
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-11 16:20:08 +0530

    30 IntegrateAPI for Update Product (frontend)

--- src/components/UpdateProduct.js
+++ src/components/UpdateProduct.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 
 const UpdateProduct = ()=> {
   const [name, setName] = useState('');
@@ -8,10 +8,7 @@ const UpdateProduct = ()=> {
   const [company, setCompany] = useState('');
 
   const params = useParams();
-
-  const updateProduct = ()=> {
-    console.log(name, price, category, company);
-  }
+  const navigate = useNavigate();
 
   useEffect(()=> {
     console.log(params);
@@ -26,10 +23,21 @@ const UpdateProduct = ()=> {
     setCategory(result.category);
     setCompany(result.company);
   }
-
-  /*
-
-  */
+  
+  const updateProduct = async ()=> {
+    console.log(name, price, category, company);
+    let result = await fetch(`https://React-and-node-js-project-MERN-stack.chandrashekha42.repl.co/product/${params.id}`, {
+      method: 'Put',
+      body: JSON.stringify({name, price, category, company}),
+      headers: {
+        'Content-Type' : 'Application/json'
+      }
+    });
+    result = await result.json();
+    console.log(result);
+    
+    navigate('/');
+  };
 
   return(
     <div className = 'product'>
