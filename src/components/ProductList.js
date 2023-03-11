8ca897fec2c79e3934b4ee14d703a6e39a9f4662
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-03-11 20:09:37 +0530

    Update list on search

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
@@ -15,44 +14,52 @@ const ProductList = () => {
     );
     result = await result.json();
     setProducts(result);
-  }; 
+  };
   console.log('producsts ', products);
 
-  const deleteProduct = async (id)=> {
+  const deleteProduct = async (id) => {
     console.log(id);
-    let result = await fetch(`https://React-and-node-js-project-MERN-stack.chandrashekha42.repl.co/product/${id}`, {
-      method: "delete"
-    });
+    let result = await fetch(
+      `https://React-and-node-js-project-MERN-stack.chandrashekha42.repl.co/product/${id}`,
+      {
+        method: 'delete',
+      }
+    );
     result = await result.json();
-    if(result){
+    if (result) {
       alert('Record deleted');
       getProducts();
     }
   };
 
-  const searchHandle = async (e)=> {
-    console.log(e.target.value)
+  const searchHandle = async (e) => {
+    console.log(e.target.value);
     let key = e.target.value;
-    let result = await fetch(`https://React-and-node-js-project-MERN-stack.chandrashekha42.repl.co/search/${key}`);
-    result = await result.json();
-    if(result){
-      setProducts(result);
+    if (key) {
+      let result = await fetch(
+        `https://React-and-node-js-project-MERN-stack.chandrashekha42.repl.co/search/${key}`
+      );
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    }else{
+      getProducts();
     }
-
-  }
+  };
 
   return (
     <div className="product-list">
       <h1>Product List</h1>
 
-      <input 
-        type = 'text'
-        className = 'search-product-box'
-        placeholder = 'Search Product'
-        onChange = {searchHandle}
+      <input
+        type="text"
+        className="search-product-box"
+        placeholder="Search Product"
+        onChange={searchHandle}
       />
 
-      <ul className = 'list-header'>
+      <ul className="list-header">
         <li>S. No</li>
         <li>Name</li>
         <li>Price</li>
@@ -61,21 +68,23 @@ const ProductList = () => {
         <li>Operation</li>
       </ul>
 
-      {products.map((item, index) => (
-        <ul key={index}>
-          <li>{index + 1}</li>
-          <li>{item.name}</li>
-          <li>₹ {item.price}</li>
-          <li>{item.category}</li>
-          <li>{item.company}</li>
-          <li>
-            <button type = 'button' onClick = {()=> deleteProduct(item._id)}>
-              Delete
-            </button> 
-            <Link to = {'/update/' + item._id}>Update</Link>
-          </li>
-        </ul>
-      ))}
+      {
+         products.length > 0 ?  products.map((item, index) => 
+          <ul key={index}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>₹ {item.price}</li>
+            <li>{item.category}</li>
+            <li>{item.company}</li>
+            <li>
+              <button type="button" onClick={() => deleteProduct(item._id)}>
+                Delete
+              </button>
+              <Link to={'/update/' + item._id}>Update</Link>
+            </li>
+          </ul>
+        ) : <h2>No result found</h2>
+      }
     </div>
   );
 };
